52e618d06e6a1a1b1b4822db6d9e356b3c21104d
tuannghiapham2210 <tuannghiapham2210@gmail.com>
2026-02-20 15:34:46 +0700

    update

---
+++ BST_AVL.cpp
@@ -0,0 +1,18 @@
+//coded by Pham Tuan Nghia (25021912)
+//QH-2025-I/CQ-I-CS2 (UET - VNU)
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+
+class BST{
+    private:
+        
+};
+
+
+int main(){
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 
-void modify(int &val){
-    val = 9;
-}
-
-int& display(int &val){
-    return val;
-}
-
 int main(){
-    int val = 10;
-    display(val) = 100;
-    std::cout << val;
+
     return 0;
 }
